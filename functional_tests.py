e2503645826c037a626dc44fa087bf2ab0f72e9c
André da Cunha Melo Tolentino <AndreTolentino@MacBook-Air-de-Bruno.local>
2021-12-28 22:03:44 -0300

    First code working with one insert line

--- functional_tests.py
+++ functional_tests.py
@@ -36,21 +36,36 @@ class NewVisitorTest(unittest.TestCase):
 
         # Quando ela tecla enter, a página é atualizada, e agora a pagina lista
         # "1: Buy peacock feathers"como um item em uma lista de tarefas
-        inputbox.send_keys('Buy peacock feathers')
-        time.sleep(1)
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
 
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
-        self.assertTrue(
-            any(row.text == '1: Buy peacock feathers'for row in rows),
-            "New to do item did not appear in table"
-        )
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
 
         # Ainda continua havendo uma caixa de texto convidando-a a acrescentar
         # outro item. Ela insere Use peacock feathers to make a fly"
         # (Usar penas de pavão para fazer um fly - Edith é bem metódica)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        # A página então é atualizada novamente e agora mostra os dois itens em sua lista
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        self.assertIn(
+            '2: Use peacock feathers to male a fly',
+            [row.text for row in rows]
+        )
+
+        # Edith se pergunta se o site lembrará de sua lista. Então ela nota
+        # que o site gerou um URL único para ela -- há um pequeno
+        # texo explicativo para isso
         self.fail('Finish the test')
 
+        # Ela acessa esse URL - sua lista de tarefas continua lá
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
